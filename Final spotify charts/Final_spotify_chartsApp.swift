1c2ab3014a0ef09e736bc14c02e3fa844ea7aff5
Evan Zona <evanbzona@gmail.com>
2022-03-30 10:32:36 -0500

    Initial Commit

---
+++ Final spotify charts/Final_spotify_chartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_spotify_chartsApp.swift
+//  Final spotify charts
+//
+//  Created by Student on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Final_spotify_chartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
